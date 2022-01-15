9e930b4e36e681f010d94b03cd8990216dbe45e8
Ben Ward <benjaminalexw@protonmail.com>
2022-01-15 15:12:30 -0600

    Remove print statements

--- hangman.v
+++ hangman.v
@@ -1,11 +1,9 @@
 import os
 fn main(){
-	f := os.read_lines('input.txt') ?
+	f := os.read_lines('input.txt') or {panic(err.msg) } 
 	mut s := 0
 	mut i := 0
 	for i<f.len-1{
-		println('--$i--||--')
-		println(f[i])
 		if f[i].int()<f[i+1].int(){
 			s++
 		}
